971fa92b3a3907a242c3946a17ff7a1e2e7a1c3e
Aishwarya C <chan.aishwarya@gmail.com>
2024-01-31 11:46:03 +0530

    Snake and Ladder problem

---
+++ ladder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake_Ladder
+{
+    internal class Program
+    {
+        static int Uc1()
+        {
+            int position = 0;
+            return position;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Single player start at position= {Uc1()}");
+            Console.ReadLine();
+        }
+    }
+}
